094d80fa86c7096e94ec744ff767d57b89787062
Letícia Keuffer <lekeuffer@gmail.com>
2026-04-13 23:09:47 -0300

    add config auth, inj dep, estudos

---
+++ src/ApiGateway.Auth/Extensions/AuthServiceExtensions.cs
@@ -0,0 +1,47 @@
+﻿using ApiGateway.Auth.Jwt;
+using ApiGateway.Auth.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiGateway.Auth.Extensions
+{
+    public static class AuthServiceExtensions
+    {
+        public static IServiceCollection AddGatewayAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtOptions = configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>()!;
+
+            services
+                .AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority               = jwtOptions.Authority;
+                    options.Audience                = jwtOptions.Audience;
+                    options.RequireHttpsMetadata    = jwtOptions.RequireHttpsMetadata;
+
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer              = true,
+                        ValidateAudience            = false,
+                        ValidateLifetime            = true,
+                        ValidateIssuerSigningKey    = true,
+                        ClockSkew                   = TimeSpan.FromSeconds(30)
+                    };
+                });
+
+            services.AddAuthorization();
+
+            return services
+        }
+
+        public static IApplicationBuilder UseGatewayAuthentication(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseMiddleware<AuthenticationMiddleware>();
+
+            return app;
+        }
+    }
+}
--- src/ApiGateway.Auth/Middleware/AuthenticationMiddleware.cs
+++ src/ApiGateway.Auth/Middleware/AuthenticationMiddleware.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System.Security.Claims;
 
 namespace ApiGateway.Auth.Middleware
 {
     public class AuthenticationMiddleware
     {
+        // Request → [RateLimiting] → [Auth] → [Cache] → YARP → Serviço
+        /*
+        _next é o próximo middleware na fila. quando eu chamo await _next(context), 
+        é como se eu tivesse falando"pasosu nessa verificação, pode seguir pro próximo"
+        se eu não chamar o _next o request para aqui 
+        ( no caso de token inválido, ele faz um 401 e não chama o next, ent o yarp nunca vê esse request)
+        */
+
         private readonly RequestDelegate _next;
         private readonly ILogger<AuthenticationMiddleware> _logger;
 
@@ -17,6 +26,14 @@ namespace ApiGateway.Auth.Middleware
 
         public async Task InvokeAsync(HttpContext context)
         {
+            /*
+            quando eu registro um middleware (app.usemiddleware<...>()) o framework usa reflection 
+            pra enocntrar um método chamado exatamente InvokeAsync ou Invoke, que receb um httpcontext
+            dúvida trouxa -> e como o httpcontext chega aqui?
+            bom, ele é criaod pelo asp.net no momento que chega uma requisição http, 
+            e contém tudo sobre aquele request específico (headers, body, path, query string, e também a resposta qeu vou montar)
+            aí o asp.net injeta ele no invoke/invokeasync que ele achou
+            */
             var result = await context.AuthenticateAsync();
 
             if (!result.Succeeded)
@@ -38,7 +55,7 @@ namespace ApiGateway.Auth.Middleware
                 context.Request.Headers["X-User"] = username;
 
             var roles = result.Principal?.FindAll(
-                r => r.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role")
+                r => r.Type == ClaimTypes.Role)
                 .Select(r => r.Value) ?? [];
 
             context.Request.Headers["X-Roles"] = string.Join(",", roles);
