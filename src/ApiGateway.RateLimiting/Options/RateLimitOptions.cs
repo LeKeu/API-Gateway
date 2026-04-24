5ea7a00af878f39ad972893db8f7cbe6fde9c256
Letícia Keuffer <lekeuffer@gmail.com>
2026-04-23 23:16:05 -0300

    add estrutura

---
+++ src/ApiGateway.RateLimiting/Options/RateLimitOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiGateway.RateLimiting.Options
+{
+    internal class RateLimitOptions
+    {
+    }
+}
