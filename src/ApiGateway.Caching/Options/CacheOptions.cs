b4be599efdb8df7ef6d70a7ee5a8ac3c5ce569b9
Letícia Keuffer <lekeuffer@gmail.com>
2026-04-29 21:49:20 -0300

    iniciando parte de cache

---
+++ src/ApiGateway.Caching/Options/CacheOptions.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ApiGateway.Caching.Options
+{
+    public class CacheOptions
+    {
+        public const string SectionName = "Cache";
+
+        //as rotas que vou cachear e as ttls de cada
+        public Dictionary<string, RouteCacheOptions> Routes { get; set; } = new();
+    }
+
+    public class RouteCacheOptions()
+    {
+        public int TtlSeconds { get; set; } = 60;
+}
---
+++ src/ApiGateway.Core/Abstractions/ICacheProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGateway.Core.Abstractions
+{
+    public interface ICacheProvider
+    {//um pra ler, outro pra escrever
+        Task<string?> GetAsync(string key);
+        //tá como string? pq pode voltar nulo, que seria o cache miss
+        Task SetAsync(string key, string value, TimeSpan ttl);
+        // valor tá como string pq vou guardar em json
+    }
+}
