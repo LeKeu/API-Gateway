4f66513880664b9e8cf84d01f3cc859e29af7474
Letícia Keuffer <lekeuffer@gmail.com>
2026-04-29 22:04:45 -0300

    add + ocnfigs, leitura e escrita no redis, ainda estudando melhor

--- src/ApiGateway.Caching/Options/CacheOptions.cs
+++ src/ApiGateway.Caching/Options/CacheOptions.cs
@@ -6,6 +6,7 @@ namespace ApiGateway.Caching.Options
         public const string SectionName = "Cache";
 
         //as rotas que vou cachear e as ttls de cada
+        //rotas q não estou no appsettings não vão ser cacheadass!
         public Dictionary<string, RouteCacheOptions> Routes { get; set; } = new();
     }
 
---
+++ src/ApiGateway.Caching/Redis/RedisCacheProvider.cs
@@ -0,0 +1,30 @@
+﻿using ApiGateway.Core.Abstractions;
+using StackExchange.Redis;
+
+namespace ApiGateway.Caching.Redis
+{
+    public class RedisCacheProvider : ICacheProvider
+    {
+        private readonly IDatabase _db;
+
+        public RedisCacheProvider(IConnectionMultiplexer redis)// não to criando uma segunda ocnexão (pq tbm uso no rate limiying), e sim reusando já que tá registradfo como singleton
+        {
+            _db = redis.GetDatabase();
+        }
+
+        public async Task<string?> GetAsync(string key)
+        {
+            var value = await _db.StringGetAsync(key);
+
+            //o rediso tem uma conversão implícita para string? (afirmação ein)
+            //ent ele vai retornar null se a chave não existe, que seria o cache miss!
+            return value.HasValue ? value.ToString() : null;
+        }
+
+        public async Task SetAsync(string key, string value, TimeSpan ttl)
+        {
+            //aqui eu to setando o cache q que quero, com o valor na chave específica pra ser apagado em ttl segundos
+            await _db.StringSetAsync(key, value, ttl);
+        }
+    }
+}
